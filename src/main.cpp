67efe08adbf5a5a606417518d33ecb8a4bbff833
Allie <isaacglewis@gmail.com>
2025-05-04 17:20:21 +0100

    silly game haha

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,31 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <array>
+#include <cstdlib>
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
+    auto window = sf::RenderWindow(sf::VideoMode({640u, 400u}), "Can't catch me!", sf::Style::Default);
     window.setFramerateLimit(144);
+
+    
+    int dx = sf::VideoMode::getDesktopMode().size.x;
+    int dy = sf::VideoMode::getDesktopMode().size.y;
+    
+    int grid[3][3][2];
+    
+    for(int x = 1; x < 4; x++){
+        for(int y = 1; y < 4; y++){
+            
+            grid[x-1][y-1][0] = (int)std::round((float)x / (float)4 * dx - 0.5 * window.getSize().x) ;
+            grid[x-1][y-1][1] = (int)std::round((float)y / (float)4 * dy - 0.5 * window.getSize().y);
+        }
+    }
+
+    
+
+
+    window.setPosition(sf::Vector2i(grid[0][0][0], grid[0][0][1]));
 
     while (window.isOpen())
     {
@@ -13,8 +35,28 @@ int main()
             {
                 window.close();
             }
-        }
+            else if(const auto* keyPressed = event->getIf<sf::Event::KeyPressed>()){
+                if(keyPressed->scancode == sf::Keyboard::Scancode::Escape)
+                    window.close();
+                else if(keyPressed->scancode == sf::Keyboard::Scancode::Space){
+                    int nx = rand() % 3;
+                    int ny = rand() % 3;
+                    window.setPosition(sf::Vector2i(grid[nx][ny][0], grid[nx][ny][1]));
+                }
+            }
+            else if(const auto* hover = event->getIf<sf::Event::MouseEntered>()){
+                int nx, ny;
+                
+                do{
+                    nx = rand() % 3;
+                    ny = rand() % 3;
+                } while(grid[nx][ny][0] == window.getPosition().x && grid[nx][ny][1] == window.getPosition().y);
 
+                window.setPosition(sf::Vector2i(grid[nx][ny][0], grid[nx][ny][1]));
+            }
+
+        
+        }
         window.clear();
         window.display();
     }
