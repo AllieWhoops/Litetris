71157e8bb9422acef52c4546a9a50d65b09c81ec
Allie <isaacglewis@gmail.com>
2025-06-30 11:21:32 +0100

    uhh

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,8 @@
 
 float gameTickLength = DEFAULT_GAME_TICK;
 
+const sf::Font font("./Bytesized-Regular.ttf");
+
 
 int main()
 {
@@ -28,95 +30,114 @@ int main()
     time_t lastTickTime = time(NULL);
     time_t lastEngineTickTime = time(NULL);
 
+    bool isGameOngoing = true;
+
     while (window.isOpen())
     {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
+        window.clear();
+
+        if(isGameOngoing){   
+            while (const std::optional event = window.pollEvent())
             {
-                window.close();
-            }
-            
-            else if(const auto* keyPressed = event->getIf<sf::Event::KeyPressed>()){
-                if(keyPressed->scancode == sf::Keyboard::Scancode::R){
-                    if(myGrid.CheckMoveLegal(newShape.TryRotate(), newShape)){
-                        myGrid.ClearSpaces(newShape.GetCoords());
-                        newShape.Rotate();
-                        myGrid.AddShape(newShape);
-                    }
-                    else{
-                        std::cout << "Illegal\n";
-                    }  
-
-                    //std::cout << myBag.GetShape();
-                    lastTickTime = time(NULL);
-                }
-                if(keyPressed->scancode == sf::Keyboard::Scancode::S){
-                    if(myGrid.CheckMoveLegal(newShape.TryMove(Tetris::Shapes::Direction::Down), newShape))
-                    {   
-                        myGrid.ClearSpaces(newShape.GetCoords());
-                        newShape.Move(Tetris::Shapes::Direction::Down);
-                        
-                        myGrid.AddShape(newShape);
-                    }
-                    else{
-                        std::cout << "Illegal\n";
-                    }
-                    lastTickTime = time(NULL);
-                }
-
-                if(keyPressed->scancode == sf::Keyboard::Scancode::A){
-                    if(myGrid.CheckMoveLegal(newShape.TryMove(Tetris::Shapes::Direction::Left), newShape))
-                    {   
-                        myGrid.ClearSpaces(newShape.GetCoords());
-                        newShape.Move(Tetris::Shapes::Direction::Left);
-                        
-                        myGrid.AddShape(newShape);
-                    }
-                    else{
-                        std::cout << "Illegal\n";
-                    }
-                    lastTickTime = time(NULL);
-                }
-                if(keyPressed->scancode == sf::Keyboard::Scancode::D){
-                    if(myGrid.CheckMoveLegal(newShape.TryMove(Tetris::Shapes::Direction::Right), newShape))
-                    {   
-                        myGrid.ClearSpaces(newShape.GetCoords());
-                        newShape.Move(Tetris::Shapes::Direction::Right);
-                        
-                        myGrid.AddShape(newShape);
-                    }
-                    else{
-                        std::cout << "Illegal\n";
-                    }
-                    lastTickTime = time(NULL);
-                }
-                    
-            }
-        }
-        time_t now = time(NULL);
-        if(difftime(now, lastTickTime) > gameTickLength){ // DoGameTick
-            lastTickTime = now;
-            if(myGrid.CheckMoveLegal(newShape.TryMove(Tetris::Shapes::Direction::Down), newShape))
-            {   
-                myGrid.ClearSpaces(newShape.GetCoords());
-                newShape.Move(Tetris::Shapes::Direction::Down);
-                
-                myGrid.AddShape(newShape);
-            }
-            else{
-                if(!myGrid.CheckIfGameOver(newShape)){
-                    myGrid.AddShape(newShape);
-                    newShape = myBag.GetShape();
-                    myGrid.DoLineClears();
-                }
-                else{
+                if (event->is<sf::Event::Closed>())
+                {
                     window.close();
                 }
+                
+                else if(const auto* keyPressed = event->getIf<sf::Event::KeyPressed>()){
+                    if(keyPressed->scancode == sf::Keyboard::Scancode::R){
+                        if(myGrid.CheckMoveLegal(newShape.TryRotate(), newShape)){
+                            myGrid.ClearSpaces(newShape.GetCoords());
+                            newShape.Rotate();
+                            myGrid.AddShape(newShape);
+                        }
+                        else{
+                            std::cout << "Illegal\n";
+                        }  
+
+                        //std::cout << myBag.GetShape();
+                        lastTickTime = time(NULL);
+                    }
+                    if(keyPressed->scancode == sf::Keyboard::Scancode::S){
+                        if(myGrid.CheckMoveLegal(newShape.TryMove(Tetris::Shapes::Direction::Down), newShape))
+                        {   
+                            myGrid.ClearSpaces(newShape.GetCoords());
+                            newShape.Move(Tetris::Shapes::Direction::Down);
+                            
+                            myGrid.AddShape(newShape);
+                        }
+                        else{
+                            std::cout << "Illegal\n";
+                        }
+                        lastTickTime = time(NULL);
+                    }
+
+                    if(keyPressed->scancode == sf::Keyboard::Scancode::A){
+                        if(myGrid.CheckMoveLegal(newShape.TryMove(Tetris::Shapes::Direction::Left), newShape))
+                        {   
+                            myGrid.ClearSpaces(newShape.GetCoords());
+                            newShape.Move(Tetris::Shapes::Direction::Left);
+                            
+                            myGrid.AddShape(newShape);
+                        }
+                        else{
+                            std::cout << "Illegal\n";
+                        }
+                        lastTickTime = time(NULL);
+                    }
+                    if(keyPressed->scancode == sf::Keyboard::Scancode::D){
+                        if(myGrid.CheckMoveLegal(newShape.TryMove(Tetris::Shapes::Direction::Right), newShape))
+                        {   
+                            myGrid.ClearSpaces(newShape.GetCoords());
+                            newShape.Move(Tetris::Shapes::Direction::Right);
+                            
+                            myGrid.AddShape(newShape);
+                        }
+                        else{
+                            std::cout << "Illegal\n";
+                        }
+                        lastTickTime = time(NULL);
+                    }
+                        
+                }
+            }
+            time_t now = time(NULL);
+            if(difftime(now, lastTickTime) > gameTickLength){ // DoGameTick
+                lastTickTime = now;
+                if(myGrid.CheckMoveLegal(newShape.TryMove(Tetris::Shapes::Direction::Down), newShape))
+                {   
+                    myGrid.ClearSpaces(newShape.GetCoords());
+                    newShape.Move(Tetris::Shapes::Direction::Down);
+                    
+                    myGrid.AddShape(newShape);
+                }
+                else{
+                    if(!myGrid.CheckIfGameOver(newShape)){
+                        myGrid.AddShape(newShape);
+                        newShape = myBag.GetShape();
+                        myGrid.DoLineClears();
+                    }
+                    else{
+                        isGameOngoing = false;
+                    }
+                }
+            }
+        }
+        else{
+            sf::Text text(font, "Game Over. Press Esc to Quit.");
+            text.setCharacterSize(28);
+            window.draw(text);
+            while (const std::optional event = window.pollEvent())
+            {
+                if(const auto* keyPressed = event->getIf<sf::Event::KeyPressed>()){
+                    if(keyPressed->scancode == sf::Keyboard::Scancode::Escape){
+                        window.close();
+                    }
+                }
             }
         }
 
-        window.clear();
+
 
         Tetris::Draw::DrawGuidelines(window);
         
